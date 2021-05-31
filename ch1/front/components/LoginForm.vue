8bb66be825083dcf774ab9d8d4656e5b5cf39830
heeo <heeo.8986@kakaocorp.com>
2021-05-31 20:30:07 +0900

    1-8. 폼 검증하기

--- ch1/front/components/LoginForm.vue
+++ ch1/front/components/LoginForm.vue
@@ -1,20 +1,24 @@
 <template>
   <v-container>
     <v-card>
-      <v-form>
+      <v-form ref="form" v-model="valid" @submit.prevent="onSubmitForm">
         <v-container>
-          <v-text-field 
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
             label="이메일"
             type="email"
             required
           />
-          <v-text-field 
+          <v-text-field
+            v-model="password"
+            :rules="passwordRules"
             label="비밀번호"
             type="password"
             required
           />
-          <v-btn color="green" type="submit">로그인</v-btn>
-          <v-btn nuxt to="signUp">회원가입</v-btn>
+          <v-btn color="green" type="submit" :disabled="!valid">로그인</v-btn>
+          <v-btn nuxt to="/signUp">회원가입</v-btn>
         </v-container>
       </v-form>
     </v-card>
@@ -22,10 +26,29 @@
 </template>
 
 <script>
-export default {
-  
-}
+  export default {
+    data() {
+      return {
+        valid: false,
+        email: '',
+        password: '',
+        emailRules: [
+          v => !!v || '이메일은 필수입니다.',
+          v => /.+@.+/.test(v) || '이메일이 유효하지 않습니다.',
+        ],
+        passwordRules: [
+          v => !!v || '비밀번호는 필수입니다.',
+        ],
+      };
+    },
+    methods: {
+      onSubmitForm() {
+        this.$refs.form.validate();
+      }
+    }
+  }
 </script>
 
 <style>
+
 </style>
